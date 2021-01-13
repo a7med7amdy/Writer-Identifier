2aeeafd66949ad02a1f4b701b3637d727732d3c3
Ahmed Hamdy <32201440+a7med7amdy@users.noreply.github.com>
2021-01-13 22:58:13 +0200

    Create Line Cutter.py

---
+++ Line Cutter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+gray = cv2.imread('formsA-D/a02-111.png')
+edges = cv2.Canny(gray,90,100,apertureSize = 3)
+minLineLength=100
+lines = cv2.HoughLinesP(image=edges,rho=1,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=minLineLength,maxLineGap=0)
+
+linesNew=[ line[0][1] for line in lines if line[0][1]==line[0][3]]
+linesNew=sorted(linesNew)
+finalList=[linesNew[0]]
+for i in range(1,len(linesNew)):
+    if linesNew[i]-linesNew[i-1]>=100:
+        finalList.append(linesNew[i])
+io.imshow(gray[finalList[1]:finalList[2],:])
+gray=gray[finalList[1]:finalList[2],:]
